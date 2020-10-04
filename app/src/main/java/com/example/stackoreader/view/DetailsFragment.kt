4e7fe4bd4e3645c1300fe5c4efa80d7f1cd71ec9
piter <piter@dummy.com>
2020-10-05 00:52:27 +0200

    Webview

--- app/src/main/java/com/example/stackoreader/view/DetailsFragment.kt
+++ app/src/main/java/com/example/stackoreader/view/DetailsFragment.kt
@@ -1,13 +1,19 @@
 package com.example.stackoreader.view
 
+import android.R
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavArgs
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.stackoreader.databinding.FragmentDetailsBinding
+
 
 class DetailsFragment : Fragment() {
 
@@ -20,7 +26,11 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        binding.urlText.text = args.item.link
+
+        binding.webview.webViewClient = WebViewClient()
+        binding.webview.loadUrl(args.item.link)
+        binding.webview.settings.javaScriptEnabled = true
+
         return binding.root
     }
 }
